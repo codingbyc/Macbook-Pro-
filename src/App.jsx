1c0190e5533c8222e56046dde4a2a55c6a963497
codingbyc <artist.chirayu@gmail.com>
2024-08-31 11:38:57 +0530

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,30 @@ import MacContainer from "./MacContainer";
 const App = () => {
   return (
     <div className="w-full h-screen font-['helvetica']">
-      <div className="navbar line flex gap-7 pt-10 pb-3 absolute top-0 left-1/2 -translate-x-1/2">
+      <div className="navbar line flex gap-8 pt-10 pb-3 absolute top-0 left-1/2 -translate-x-1/2">
         {[
-          "iPhone",
-          "iPad",
-          "Services",
+          "Store",
           "Mac",
-          "iOS",
-          "Accesories",
-          "Products",
-          "iPhone",
           "iPad",
-          "Services",
-          "Mac",
-          "iOS",
+          "iPhone",
+          "iCloud",
+          "Watch",
+          "AirPods",
+          "TV&Home",
+          "Entertainment",
           "Accesories",
-          "Products",
+          "Support",
         ].map((e) => (
-        <a href="" className="text-white font-[400] text-sm capitalize">
+        <a href="" className="text-white font-[400] text-sm ">
          {e}
         </a>
         ))} ;
       </div>
       <div className="absolute flex flex-col items-center text-white top-24 left-1/2 -translate-x-1/2 ">
-        <h3 className="masked text-7xl tracking-tighter font-[600] ">macbook pro.</h3>
-        <h5>Oh so pro!</h5>
-        <p className="text-center w-2/3">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum
-          minima quae recusandae omnis.
+        <h3 className="masked text-7xl tracking-tighter font-[600] ">MacBook Pro</h3>
+        <h5 className="font-[200]">Mind-blowing,  Head-turning </h5>
+        <p className="font-[200]  text-center w-2/3">
+           Buy MacBook Pro starting at ₹158900.00 with education savings. And now up to 30 Sep, get AirPods.
         </p>
       </div>
       <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
